385b50c1d63a96b3057bf5d32b53f0120bf6d487
Sui / SuicideMachine <SuiMachine@users.noreply.github.com>
2025-06-28 02:39:50 +0200

    Add utils

---
+++ AIMessageUtils.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace SuiBotAI
+{
+	public static class AIMessageUtils
+	{
+		public static string AppendDateTimePrefix(string message)
+		{
+			System.Globalization.CultureInfo globalizationOverride = new System.Globalization.CultureInfo("en-US");
+
+			return $"[DATETIME: Local {DateTime.Now.ToString("yyy-MM-dd", globalizationOverride)} {DateTime.Now:HHH:mm:ss} | UTC {DateTime.UtcNow.ToString("yyy-MM-dd", globalizationOverride)} {DateTime.UtcNow:HH:mm:ss}Z]: {message}";
+		}
+	}
+}
